8bffbf508f965b3b6a393f2cbdac058a3ab92273
bertrand lne <ad9000@hotmail.fr>
2025-02-22 16:28:11 +0100

    maj

--- Bourse/import requests.py
+++ Bourse/import requests.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://live.euronext.com/fr/product/equities/FR0000133308-XPAR"
+url = "https://live.euronext.com/fr/product/equities/FR0000051732-XPAR"
 
 x = requests.get(url)
 soup = BeautifulSoup(x.text, "html.parser")
